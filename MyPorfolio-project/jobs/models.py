f897ebdb5cddaf8382d3f963600fd5e6fa9e69b2
Segrey_Davydov <sergey_davydov2@epam.com>
2019-12-11 14:47:06 +0600

    add navigation

--- MyPorfolio-project/jobs/models.py
+++ MyPorfolio-project/jobs/models.py
@@ -4,3 +4,7 @@ from django.db import models
 class Job(models.Model):
     image = models.ImageField(upload_to="image/")
     summary = models.CharField(max_length=200)
+
+
+    def __srt__(self):
+        return self.summary
--- MyPorfolio-project/jobs/views.py
+++ MyPorfolio-project/jobs/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Job
 
 # Create your views here.
 def main(request):
     jobs = Job.objects
     return render(request, 'jobs/index.html', {'jobs': jobs})
+
+def details(request, job_id):
+    job_detail = get_object_or_404(Job, pk=job_id)
+    return render(request, 'jobs/details.html',{'job': job_detail})
