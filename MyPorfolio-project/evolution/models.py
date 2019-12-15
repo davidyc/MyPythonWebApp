c2b21272eac9755f3918b5fcfdb3213ada801e62
Davidyc <davydovsergey.sd@gmail.com>
2019-12-15 21:38:34 +0600

    add  data to db part 2

--- MyPorfolio-project/evolution/models.py
+++ MyPorfolio-project/evolution/models.py
@@ -5,16 +5,28 @@ class Player(models.Model):
     image = models.ImageField(upload_to="image/")
     name = models.CharField(max_length=25)
 
+    def __str__(self):
+        return self.name
+
 class Section(models.Model):
     title = models.CharField(max_length=25)
+
+    def __str__(self):
+        return self.title
 
 class Theme(models.Model):
     title = models.CharField(max_length=25)
     section = models.ForeignKey(Section, on_delete=models.CASCADE)
 
+    def __str__(self):
+        return self.title
+
 class Phase(models.Model):
     number = models.IntegerField()
-    startDate = models.DateField(auto_now=True)
+    startDate = models.DateField()
     finishDate = models.DateField()
     theme = models.ForeignKey(Theme, on_delete=models.CASCADE)
     player = models.ForeignKey(Player, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return "Phase {} {} ".format(str(self.number),self.theme.title)
