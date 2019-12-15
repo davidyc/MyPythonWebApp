1360f1e294800a43042cf10892518c48004e9471
Davidyc <davydovsergey.sd@gmail.com>
2019-12-15 13:39:59 +0600

    add players for main page

--- MyPorfolio-project/evolution/views.py
+++ MyPorfolio-project/evolution/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
+from .models import Player
 
 # Create your views here.
 def main(request):
-    return render(request, 'evo/index.html',)
+    players = Player.objects
+    return render(request, 'evo/index.html', {'players': players})
+
+    
+  
+   
