19efedec9f43866394d9e879ed0e8a9e515bf5a4
Davidyc <davydovsergey.sd@gmail.com>
2019-12-16 01:34:09 +0600

    add tmp soluioon phases

--- MyPorfolio-project/evolution/views.py
+++ MyPorfolio-project/evolution/views.py
@@ -1,12 +1,17 @@
+from datetime import date
 from django.shortcuts import render
-from .models import Player
+from .models import Player, Section, Theme, Phase, PhaseTheme
 
 # Create your views here.
 def main(request):
+    today = date.today()
     players = Player.objects
-    return render(request, 'evo/index.html', {'players': players})
+    phase = Phase.objects.get(id=2)
+    return render(request, 'evo/index.html', {'players': players, 'phase':phase})
 
 def themes(request):    
-    return render(request, 'evo/themes.html')   
+    sections = Section.objects
+    themes = Theme.objects
+    return render(request, 'evo/themes.html', {'sections' : sections, 'themes' : themes })   
   
    
