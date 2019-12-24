dae81c1dcd614936d4404d2aedd522b1d4e8badb
Davidyc <davydovsergey.sd@gmail.com>
2019-12-24 23:55:08 +0600

    first response

--- MyPorfolio-project/simplebot/views.py
+++ MyPorfolio-project/simplebot/views.py
@@ -1,5 +1,21 @@
 from django.shortcuts import render
+import apiai, json
 
 # Create your views here.
 def main(request):   
+    print(_getresponse('Привет'))
     return render(request, 'bot/index.html')
+
+
+def _getresponse(text):
+    request = apiai.ApiAI('710a5fa1d9cf4d14af93e3eacf38294a').text_request()
+    request.lang = 'ru'
+    request.session_id = 'SimpleBot'
+    request.query = text
+    responsejson = json.loads(request.getresponse().read().decode('utf-8'))
+    response = responsejson['result']['fulfillment']['speech']
+
+    if response:
+        return response
+    else:
+        return "Я не понимаю"
