4e890df575588637bf1e249d2bbaf8ac0e1166f5
Davidyc <davydovsergey.sd@gmail.com>
2020-01-09 23:46:23 +0600

    templetes for menu

--- MyPorfolio-project/mymenu/views.py
+++ MyPorfolio-project/mymenu/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseNotFound
 
 def main(request):
-    return HttpResponse('<h1>First view  for my beloved wife</h1>')
+    return render(request, 'menu/index.html')
 
 
