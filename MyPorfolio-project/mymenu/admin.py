a84509b6703643402070cf58fb0032ca5364dfcf
Davidyc <davydovsergey.sd@gmail.com>
2020-01-10 12:18:49 +0600

    Models for mymenu

--- MyPorfolio-project/mymenu/admin.py
+++ MyPorfolio-project/mymenu/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Product, Category, Dish, Ingredient, Week, ListDishesWeek
+
 
 # Register your models here.
+admin.site.register(Product)
+admin.site.register(Category)
+admin.site.register(Dish)
+admin.site.register(Ingredient)
+admin.site.register(Week)
+admin.site.register(ListDishesWeek)
--- MyPorfolio-project/mymenu/models.py
+++ MyPorfolio-project/mymenu/models.py
@@ -1,3 +1,41 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.name
+
+class Category(models.Model):
+    name = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.name
+   
+class Dish(models.Model):
+    name = models.CharField(max_length=50)   
+    dishcategory = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+class Ingredient(models.Model):
+    dish = models.ForeignKey(Dish, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return "{} для {}".format(self.dish, self.product)
+
+
+class Week(models.Model):
+    name = models.CharField(max_length=50)      
+
+    def __str__(self):
+        return "Неделя {}".format(self.name)
+
+class ListDishesWeek(models.Model):
+    dish = models.ForeignKey(Dish, on_delete=models.CASCADE)
+    week = models.ForeignKey(Week, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return "Блюдо {} для {} ".format(self.dish, self.week)
