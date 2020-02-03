8af000380106be6e01973edae67d518dc181bb54
Segrey_Davydov <sergey_davydov2@epam.com>
2020-02-03 16:49:58 +0600

    my menu add some unit tests example

--- MyPorfolio-project/mymenu/tests.py
+++ MyPorfolio-project/mymenu/tests.py
@@ -1,3 +1,22 @@
 from django.test import TestCase
+from .forms import ProductForm
+from .models import Product
+#import hashlib
+
+
 
 # Create your tests here.
+class FirstTest(TestCase):
+
+# тест на использование шаюлона 
+    def test_temple(self):
+        response = self.client.get("/jobs/mymenu/singup")
+        self.assertTemplateUsed(response, "menu/reg.html")
+
+# тест на возращение формы        
+    def test_form(self):
+        form = ProductForm(data={"name":"UnittestProduct"})
+        self.assertTrue(form.is_valid())
+    
+
+  
