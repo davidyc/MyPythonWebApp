13a5556f5d560473510c3aae2291fc8eefcfb971
Sergey Davydov2 <Sergey_Davydov2@epam.com>
2020-08-14 22:46:29 +0600

    add postgrsql

--- MyPorfolio-project/MyPorfolio/settings.py
+++ MyPorfolio-project/MyPorfolio/settings.py
@@ -81,10 +81,15 @@ WSGI_APPLICATION = 'MyPorfolio.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'Test',
+        'USER': 'postgres',
+        'PASSWORD': '12345',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
+
 
 
 # Password validation
