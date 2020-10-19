1fcb1bd81d9b7a949ed148a4db1b6345a293c3eb
Sergey Davydov2 <Sergey_Davydov2@epam.com>
2020-10-19 22:42:32 +0600

    add new funcs t voite

--- MyPorfolio-project/voice/urls.py
+++ MyPorfolio-project/voice/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path('', voice.views.main, name='voite'),
     path('voite', voice.views.voite, name='voited'),
     path('showall', voice.views.show_allvoites, name='showall'),   
+    path('showcount', voice.views.show_count, name='showcount'),   
     path('delete/<int:voice_id>', voice.views.detele_voice, name='deletevoice'),
     
 ] 
